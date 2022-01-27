f7705c6c126551f09e9bf9cafcc2593afb1b5f96
Miranda <mirandayaxley@att.net>
2022-01-26 23:45:29 -0500

    Homework 2 - Player Fixes
    -Stopped the player from rotating
    -Fixed an issue with player movement
    -Limited camera vertical rotation

--- ProceduralProject/Assets/Scripts/CameraController.cs
+++ ProceduralProject/Assets/Scripts/CameraController.cs
@@ -6,6 +6,10 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
 
+    private float upDownAngle = 0;
+
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +21,31 @@ public class CameraController : MonoBehaviour
     {
         transform.position = player.transform.position;
 
+        speed = Time.deltaTime * 100;
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * 100, Space.World);
+            transform.Rotate(new Vector3(0, -1, 0) * speed, Space.World);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 100, Space.World);
+            transform.Rotate(new Vector3(0, 1, 0) * speed, Space.World);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * 100, Space.Self);
+            if(upDownAngle < 88)
+            {
+                upDownAngle += speed;
+                transform.Rotate(new Vector3(1, 0, 0) * speed, Space.Self);
+            }
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * 100, Space.Self);
+            if (upDownAngle > -88)
+            {
+                upDownAngle -= speed;
+                transform.Rotate(new Vector3(-1, 0, 0) * speed, Space.Self);
+            }
         }
     }
 }
--- ProceduralProject/Assets/Scripts/PlayerController.cs
+++ ProceduralProject/Assets/Scripts/PlayerController.cs
@@ -23,45 +23,27 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            //body.AddForce(forward);
-
             forward.y = 0;
-            body.position += forward * Time.deltaTime * 10;
+            body.position += forward.normalized * Time.deltaTime * 6;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            //body.AddForce(-forward);
-
             forward.y = 0;
-            body.position -= forward * Time.deltaTime * 10;
+            body.position -= forward.normalized * Time.deltaTime * 6;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            //body.AddForce(right);
-
             right.y = 0;
-            body.position += right * Time.deltaTime * 10;
+            body.position += right.normalized * Time.deltaTime * 6;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            //body.AddForce(-right);
-
             right.y = 0;
-            body.position -= right * Time.deltaTime * 10;
+            body.position -= right.normalized * Time.deltaTime * 6;
         }
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.Space))
         {
-            //body.AddForce(up);
-
-            //body.position += up * Time.deltaTime * 10;
-
             body.position += new Vector3(0, 1, 0) * Time.deltaTime * 10;
-        }
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            //body.AddForce(-up);
-
-            //body.position -= up * Time.deltaTime * 20;
         }
     }
 }
